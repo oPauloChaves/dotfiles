a635dab1ffc191e8b7fef37864babb428f1daea8
Paulo Chaves <opaulochaves@gmail.com>
2017-09-28 15:01:17 -0300

    add default tern config

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ rm -f .bash_aliases
 rm -f .bashrc
 rm -f .fzf.bash
 rm -f .profile
+rm -f .tern-config
 
 if [ -d "$HOME/.config/nvim" ] ; then
 	rm -rf "$HOME/.config/nvim"
@@ -13,5 +14,6 @@ ln -s ~/.config/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.config/dotfiles/.bashrc ~/.bashrc
 ln -s ~/.config/dotfiles/.fzf.bash ~/.fzf.bash
 ln -s ~/.config/dotfiles/.profile ~/.profile
+ln -s ~/.config/dotfiles/.tern-config ~/.tern-config
 ln -s ~/.config/dotfiles/init.vim ~/.config/nvim/init.vim
 
