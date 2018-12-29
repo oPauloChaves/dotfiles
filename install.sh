790c4c733c677555c4d339092d3402857c93423d
Paulo Chaves <opaulochaves@gmail.com>
2018-12-29 00:04:18 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,56 +1,25 @@
 #!/bin/bash
 
-if [ -f "$HOME/.bashrc" ]; then
-  mv "$HOME/.bashrc" "$HOME/.old-bashrc"
-fi
+[ -f ~/.bashrc ] && [ ! -f ~/.old-bashrc ] && mv ~/.bashrc ~/.old-bashrc
+[ -f ~/.zshrc ] && [ ! -f ~/.old-zshrc ] && mv ~/.zshrc ~/.old-zshrc
+[ -f ~/.profile ] && [ ! -f ~/.old-profile ] && mv ~/.profile ~/.old-profile
+[ -f ~/.tern-config ] && [ ! -f ~/.old-tern-config ] && mv ~/.tern-config ~/.old-tern-config
+[ -f ~/.tmux.conf ] && [ ! -f ~/.old-tmux.conf ] && mv ~/.tmux.conf ~/.old-tmux.conf
+[ -f ~/.inputrc ] && [ ! -f ~/.old-inputrc ] && mv ~/.inputrc ~/.old-inputrc
+[ -d ~/.config/nvim ] && [ ! -d ~/.config/old-nvim ] && mv ~/.config/nvim ~/.config/old-nvim
 
-if [ -f "$HOME/.zshrc" ]; then
-  mv "$HOME/.zshrc" "$HOME/.old-zshrc"
-fi
-
-if [ -f "$HOME/.profile" ]; then
-  mv "$HOME/.profile" "$HOME/.old-profile"
-fi
-
-if [ -f "$HOME/.tern-config" ]; then
-  mv "$HOME/.tern-config" "$HOME/.old-tern-config"
-fi
-
-if [ -f "$HOME/.tmux.conf" ]; then
-  mv "$HOME/.tmux.conf" "$HOME/.old-tmux.conf"
-fi
-
-if [ -f "$HOME/.inputrc" ]; then
-  mv "$HOME/.inputrc" "$HOME/.old-inputrc"
-fi
-
-if [ ! -s "$HOME/.config/nvim" ]; then
-  mkdir "$HOME/.config/nvim"
-else
-  [[ -s "$HOME/.config/nvim/init.vim" ]] && mv "$HOME/.config/nvim/init.vim" "$HOME/.config/nvim/old-init.vim"
-fi
-
-if [ ! -d "$HOME/.zsh.d" ]; then
-  mkdir "$HOME/.zsh.d"
-fi
+[ -d ~/.zsh.d ] || mkdir ~/.zsh.d
 
 for file in zsh/*; do
-  ln -s "$(pwd)/$file" "$HOME/.zsh.d/$(basename $file)"
+  [ -L ~/.zsh.d/$(basename $file) ] || ln -s "$(pwd)/$file" ~/.zsh.d/$(basename $file)
 done
 
-ln -s "$HOME/code/dotfiles/.zshrc" "$HOME/.zshrc"
-ln -s "$HOME/code/dotfiles/.bashrc" "$HOME/.bashrc"
-# ln -s "$HOME/dotfiles/.profile" "$HOME/.profile"
-ln -s "$HOME/code/dotfiles/.inputrc" "$HOME/.inputrc"
-ln -s "$HOME/code/dotfiles/.tern-config" "$HOME/.tern-config"
-
-ln -s "$HOME/code/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
-ln -s "$HOME/code/dotfiles/nvim/autoload" "$HOME/.config/nvim/autoload"
-ln -s "$HOME/code/dotfiles/nvim/plugged" "$HOME/.config/nvim/plugged"
-ln -s "$HOME/code/dotfiles/nvim/colors" "$HOME/.config/nvim/colors"
-ln -s "$HOME/code/dotfiles/nvim/ftdetect" "$HOME/.config/nvim/ftdetect"
-ln -s "$HOME/code/dotfiles/nvim/UltiSnips" "$HOME/.config/nvim/UltiSnips"
-
-ln -s "$HOME/code/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
+[ ! -L ~/.zshrc ] && ln -s "$(pwd)/.zshrc" ~/.zshrc
+[ ! -L ~/.bashrc ] && ln -s "$(pwd)/.bashrc" ~/.bashrc
+[ ! -L ~/.profile ] && ln -s "$(pwd)/.profile" ~/.profile
+[ ! -L ~/.inputrc ] && ln -s "$(pwd)/.inputrc" ~/.inputrc
+[ ! -L ~/.tern-config ] && ln -s "$(pwd)/.tern-config" ~/.tern-config
+[ ! -L ~/.config/nvim ] && ln -s "$(pwd)/nvim" ~/.config/nvim
+[ ! -L ~/.tmux.conf ] && ln -s "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
 
 echo "Setup done"
